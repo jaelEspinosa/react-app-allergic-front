0aa92d4cdb646fd60aad1d43f0b1211e1eb66984
fernandojcm <fernandocamacho50@gmail.com>
2022-04-23 20:59:39 +0200

    WelcomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import WelcomeComponent from './components/WelcomeComponent';
 
 function App() {
   return (
     <div className="App">
-    
+    {/* <WelcomeComponent /> */}
     </div>
   );
 }
---
+++ src/components/WelcomeComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./WelcomeComponent.css";
+import group from "../images/group.png"
+import logo from "../images/logoApplergicFigurasGiro.png"
+
+const WelcomeComponent = () => {
+  return (
+    <div className="c-wpage">
+      <img className="c-wpage__title" src={group} alt="appTitle"></img>
+
+      <img className="c-wpage__logo" src={logo} alt="appLogo"></img>
+    </div>
+  );
+};
+
+export default WelcomeComponent;

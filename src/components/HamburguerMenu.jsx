7da296026e8e11e568edc71a122195ff0e758937
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-04-29 09:18:30 +0200

    hamburguer

---
+++ src/components/HamburguerMenu.jsx
@@ -0,0 +1,28 @@
+
+
+
+const HamburguerMenu = () => {
+   return (
+       <div className="hamburger">
+        <p>X</p>
+        
+        <ul>
+        <li>Perfil</li>
+        <li>Favorito</li>
+        <li>Diario</li>
+        <li>Compartir</li>
+        <li>Traductor</li>
+        <li>Terminos</li>
+        <li>Salir</li>
+
+        </ul>
+       
+           
+       </div>
+   )
+
+}
+ 
+
+
+export default HamburguerMenu

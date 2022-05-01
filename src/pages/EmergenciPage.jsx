69d78c708848e2db2068b6d34c88e0e911ee9879
BellaMir <bellacontrerascamperos@yahoo.es>
2022-05-01 12:00:01 +0200

    cambios puerto y emergency

--- src/pages/EmergenciPage.jsx
+++ src/pages/EmergenciPage.jsx
@@ -45,42 +45,42 @@ export default function ContactPage() {
     <button className="c-startpage_return" onClick={() => navigate(-1)}>
           Volver
         </button>
-    <div class="row">
-<div class="col-12 text1">Vamos a añadir tu contacto en caso de emergencia.</div>
-<div class="col-12 text2">Nos pondremos en contacto con tu persona de confianza y/o compañia de seguros en casa de emergencia.</div>
+    <div className="row">
+<div className="col-12 text1">Vamos a añadir tu contacto en caso de emergencia.</div>
+<div className="col-12 text2">Nos pondremos en contacto con tu persona de confianza y/o compañia de seguros en casa de emergencia.</div>
 </div>
       <div>
-        <label class="row">
-          <input   class="form-control line col-12" type="text" placeholder="Nombre completo de tu contacto" {...register("nombreContactoEmergencia")} />
+        <label className="row">
+          <input   className="form-control line col-12" type="text" placeholder="Nombre completo de tu contacto" {...register("nombreContactoEmergencia")} />
         </label>
         <FormError error={errors.nombreContactoEmergencia}/>
       </div>
 
       <div>
-        <label class="row">
-          <input  class="form-control line col-12" type="text" placeholder="Correo contacto emergencia"{...register("correoContactoEmergencia")} />
+        <label className="row">
+          <input  className="form-control line col-12" type="text" placeholder="Correo contacto emergencia"{...register("correoContactoEmergencia")} />
         </label>
         <FormError error={errors.correoContactoEmergencia}/>
       </div>
 <div>
-          <label class="row">
+          <label className="row">
 
-              <input  class="form-control line col-12" type="text" placeholder="Telefono contacto emergencia"{...register("telefonoContactoEmergencia")}>
+              <input  className="form-control line col-12" type="text" placeholder="Telefono contacto emergencia"{...register("telefonoContactoEmergencia")}>
               </input>
           </label>
           <FormError error={errors.telefonoContactoEmergencia}/>
       </div>
       <div>
-          <label class="row">
+          <label className="row">
 
-        <input  class="form-control line col-12" type="text" placeholder="Compañia de seguros/ Nº de poliza"{...register("polizaSeguros")}>
+        <input  className="form-control line col-12" type="text" placeholder="Compañia de seguros/ Nº de poliza"{...register("polizaSeguros")}>
               </input>
               <FormError error={errors.polizaSeguros}/>
           </label>
       </div>
-<div class="row">
-      <button type="submit" class="btn btn-info col-12">Guardar emergencia</button>
-      <div class="col-12 stylenext">Registraré mi contacto en otro momento</div>
+<div className="row">
+      <button type="submit" className="btn btn-info col-12">Guardar emergencia</button>
+      <div className="col-12 stylenext">Registraré mi contacto en otro momento</div>
 </div>
     </form>
   );

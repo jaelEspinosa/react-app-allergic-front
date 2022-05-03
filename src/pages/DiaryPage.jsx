a6b952d89d519a31405cca0f5244eca62d0470f3
fernandojcm <fernandocamacho50@gmail.com>
2022-05-03 15:54:52 +0200

    save product

--- src/pages/DiaryPage.jsx
+++ src/pages/DiaryPage.jsx
@@ -2,11 +2,44 @@ import React, { useContext } from 'react';
 import './DiaryPage.scss';
 import { ProductContext } from '../context/ProductContext';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 const DiaryPage = () => {
-    
+
 const {product, setProduct} = useContext(ProductContext);
 console.log('esto es lo que me traigo', product)
+
+  const navigate = useNavigate
+  const location = useLocation();
+  
+  const USER_ID = sessionStorage.getItem('userID');
+  const TOKEN = sessionStorage.getItem('token');
+
+  const saveProduct = () => {
+    axios({
+      method: "put",
+      url: "http://localhost:4000/users/updateUserById/"+USER_ID,
+      data: product,
+      headers: { "Content-Type": "application/json", 'authorization' : TOKEN },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+        if (response.status === 200) { 
+          console.log(response.status)
+        }
+      })
+      .catch(function (response) {
+        //handle error
+
+        console.log(response);
+        
+      });
+  }
+
+    
+
 
 
 
@@ -22,7 +55,7 @@ console.log('esto es lo que me traigo', product)
       <h4>{product.nombre}</h4>
       </div>
       <div>
-      <Link to ='/home'><button type="submit" className="btn btn-info col-12">Guardar</button></Link>
+      <Link to = '/home'><button type="submit" className="btn btn-info col-12" onClick={() => {saveProduct()}}>Guardar</button></Link>
           
         
       </div>
